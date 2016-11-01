c8ec6dd7c2374eb82db33d007d911ff32ca47cbd
Dmytro Struk <mr.struk@list.ru>
2016-11-01 16:30:57 +0200

    Create Game.cs and Player.cs Also add to Main function some example of code for checking the work of new classes

---
+++ EconomicGame/EconomicGame/EconomicGame/Game.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomicGame
+{
+    public class Game
+    {
+        // Рівень складності
+        public int DifficultyLevel { get; private set; } 
+
+        // Якщо гру створюємо без вказаного рівня, то за замовчуванням рівень = 1
+        public Game() : this(1) { }
+
+        public Game(int DifficultyLevel)
+        {
+            this.DifficultyLevel = DifficultyLevel;
+        }
+
+        public void Start(List<Player> players)
+        {
+            // Задання початкових ресурсів (поки що немає прив'язки до рівня складності)
+            foreach(var player in players)
+            {
+                player.resources.Coal = 100;
+                player.resources.Iron_Ore = 100;
+                player.resources.Gold = 100;
+                player.resources.Wood = 100;
+                player.resources.Clay = 100;
+                player.resources.Sand = 100;
+                player.resources.Stone = 100;
+            }
+        }
+    }
+}
---
+++ EconomicGame/EconomicGame/EconomicGame/Player.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomicGame
+{
+    public class Player
+    {
+        // Ім'я гравця
+        public string Name { get; private set; }
+
+        // Ресурси гравця
+        public Resources resources;
+
+        //Якщо при створенні об'єкту ім'я не вказане - задаємо ім'я за замовчуванням
+        public Player() : this("Anonymus") { }
+
+        public Player(string Name)
+        {
+            this.Name = Name;
+            resources = new Resources();
+        }
+    }
+
+    public struct Resources
+    {
+        public int Coal; // Вугілля
+        public int Iron_Ore; // Залізна руда
+        public int Gold; // Золото
+        public int Wood; // Дерево
+        public int Clay; // Глина
+        public int Sand; // Пісок
+        public int Stone; // Камінь
+    }
+}
