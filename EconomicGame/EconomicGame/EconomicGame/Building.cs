208c9880a9a78f171c114fd717022c0360f08c89
Dmytro Struk <mr.struk@list.ru>
2016-11-01 16:55:10 +0200

    Add abstract class Building.cs

---
+++ EconomicGame/EconomicGame/EconomicGame/Building.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EconomicGame
+{
+    public abstract class Building
+    {
+        public int CurrentLevel; // Поточний рівень будівлі
+        public int MaximumLevel = 10; // Максимальний рівень будівлі
+        public int CreatingTime; // Поточний час створення будівлі
+        public int FullCreatingTime; // Тривалість створення будівлі
+        public Player Owner; // Власник будівлі
+        public int State; // Стан будівлі
+        public Resources Price; // Вартість будівлі
+    }
+
+    // Стани для будівлі
+    enum States
+    {
+        Building = 0, // Будується
+        Working = 1 // Працює (виробляє або добуває щось)
+    }
+}
